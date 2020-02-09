f8aece9a73017bde13801d4a6c00abbede0f2467
kyle074613 <kylemj@uci.edu>
2020-02-08 22:39:27 -0800

    delete api route working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -22,8 +22,29 @@ module.exports = function (app) {
         return res.json(req.body);
     });
 
-    // app.delete('/api/notes/:id', (req, res) => {
+    app.delete('/api/notes/:id', (req, res) => {
+        const id = req.params.id;
+        const notesJSON = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
 
-    // });
+        console.log(notesJSON);
+
+        for (let i = 0; i < notesJSON.length; i++) {
+
+            console.log(`id: ${id}`);
+            console.log(notesJSON[i].id);
+
+            if (notesJSON[i].id === parseInt(id)) {
+                notesJSON.splice(i, 1);
+
+                for (let i = 0; i < notesJSON.length; i++)
+                    notesJSON[i].id = i + 1;
+
+                fs.writeFileSync("./db/db.json", JSON.stringify(notesJSON));
+                return res.json(true);
+            }
+        }
+
+        return res.json(false);
+    });
 }
 
