3f467199841c12f4437566b9ad15dc2caf5dffc3
kyle074613 <kylemj@uci.edu>
2020-02-08 21:34:33 -0800

    all routes have been created and work, except delete route

--- server.js
+++ server.js
@@ -1,2 +1,16 @@
-var express = require("express");
+const express = require("express");
 
+const app = express();
+const PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Designates routes from api and html route files
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+//Starts the server
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
